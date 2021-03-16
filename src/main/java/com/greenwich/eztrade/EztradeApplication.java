4d08aa6c95d23b36ffc3a887e44c70601595b00a
mohieddinalghali <mo-alghali@hotmail.com>
2021-03-16 01:45:34 +0000

    fs

---
+++ src/main/java/com/greenwich/eztrade/EztradeApplication.java
@@ -0,0 +1,13 @@
+package com.greenwich.eztrade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EztradeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EztradeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/greenwich/eztrade/EztradeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greenwich.eztrade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EztradeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
